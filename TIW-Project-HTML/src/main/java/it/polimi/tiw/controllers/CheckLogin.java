762978d6e5bec843cc101ad34ccfe372fd4882b9
AlessandroSironi <alessandro1.sironi@mail.polimi.it>
2022-04-19 16:34:43 +0200

    Index.html, CheckLogin

--- TIW-Project-HTML/src/main/java/it/polimi/tiw/controllers/CheckLogin.java
+++ TIW-Project-HTML/src/main/java/it/polimi/tiw/controllers/CheckLogin.java
@@ -1,6 +1,8 @@
 package it.polimi.tiw.controllers;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -28,6 +30,73 @@ public class CheckLogin extends HttpServlet {
 	private Connection connection = null;
 	private TemplateEngine templateEngine;
 	
+	public CheckLogin() {
+		super();
+	}
 	
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		String usr = null;
+		String pwd = null;
+		
+		try {
+			usr = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			pwd = StringEscapeUtils.escapeJava(request.getParameter("password"));
+			
+			if (usr == null || pwd == null || usr.isEmpty() || pwd.isEmpty())
+					throw new Exception ("Error: missing or empty credential value");
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credentials");
+			return;
+		}
+		
+		UserDAO userDao = new UserDAO(connection);
+		User user = null;
+		
+		try {
+			user = userDao.checkCredentials(usr, pwd);
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unable to check credentials");
+			return;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		
+		// If user exists, add info to the session and go to Main Page. 
+		// Otherwise, show login page with error.
+		
+		String path = null;
+		
+		if (user == null) {
+			ServletContext servletContext = getServletContext();
+			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			ctx.setVariable("errorMsg", "Incorrect username or password");
+			path = "/index.html";
+			templateEngine.process(path,  ctx, response.getWriter());
+		} else {
+			request.getSession().setAttribute("user", user);
+			path = getServletContext().getContextPath() + "/home";
+			response.sendRedirect(path);
+		}
+	}
+	
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
